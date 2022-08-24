cd3a3877934cddf9e803221064c0c32e2fc6a5d2
ana777stasia <ppc1312@gmail.com>
2022-08-24 12:36:29 +0200

    set react structure app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">hello</header>
+      <a href="/" className="btn btn-primary">
+        Hello
+      </a>
+    </div>
+  );
+}
+
+export default App;
