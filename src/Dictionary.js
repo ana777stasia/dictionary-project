0e6798912e80c59b023348b50b9c5f35aa65b43c
ana777stasia <ppc1312@gmail.com>
2022-08-24 13:48:09 +0200

    added API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,18 @@
+import axios from "axios";
 import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleWordChanges(event) {
