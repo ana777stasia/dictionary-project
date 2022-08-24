47f922818514c495286097a3964dc219a18bb926
ana777stasia <ppc1312@gmail.com>
2022-08-24 13:15:30 +0200

    added search form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">hello</header>
-      <a href="/" className="btn btn-primary">
-        Hello
-      </a>
+      <div className="container">
+        <header className="App-header"><h1>Dictionary app</h1></header>
+        <main>
+          <Dictionary />
+        </main>
+      
+      </div>
     </div>
   );
 }
