0ab9ee5fad2771f32f7df3ab0176bff97195a3b0
ana777stasia <ppc1312@gmail.com>
2022-08-24 17:08:05 +0200

    rendered word definitions

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,19 @@
 import axios from "axios";
 import React, { useState } from "react";
 import "./Dictionary.css";
+import Results from "./Results";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
+  const [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data);
+    setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
@@ -28,6 +30,7 @@ export default function Dictionary() {
           onChange={handleWordChanges}
         />
       </form>
+      <Results results={results} />
     </div>
   );
 }
