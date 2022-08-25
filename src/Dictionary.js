2629cb261fab4ac3f9860071cee0f79c474240b8
ana777stasia <ppc1312@gmail.com>
2022-08-25 18:14:16 +0200

    set default search word

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,34 +4,49 @@ import "./Dictionary.css";
 import Results from "./Results";
 
 export default function Dictionary() {
-  const [keyword, setKeyword] = useState("");
+  const [keyword, setKeyword] = useState("sunset");
   const [results, setResults] = useState(null);
+  const [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
-
-  function search(event) {
-    event.preventDefault();
+  function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
   }
 
   function handleWordChanges(event) {
     setKeyword(event.target.value);
   }
 
-  return (
-    <div className="Dictionary">
-      <form onSubmit={search}>
-        <h2>What word do you want to look up?</h2>
-        <input
-          type="search"
-          placeholder="search for a word..."
-          onChange={handleWordChanges}
-        />
-      </form>
-      <Results results={results} />
-    </div>
-  );
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <form onSubmit={handleSubmit}>
+          <h2>What word do you want to look up?</h2>
+          <input
+            type="search"
+            placeholder="search for a word..."
+            onChange={handleWordChanges}
+            defaultValue="sunset"
+          />
+        </form>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading...";
+  }
 }
